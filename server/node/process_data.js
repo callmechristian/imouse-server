c4ad5c289703e173c04a4dae62e2c89f4a4e91da
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2022-09-19 15:18:15 +0200

    mathjs

--- server/node/process_data.js
+++ server/node/process_data.js
@@ -1,4 +1,5 @@
-module.exports = {
+const {pi, matrix, abs, sin, cos, sqrt, pow, atan, asin, multiply, divide} = require('mathjs');
+module.exports = {   
     processAccellerationToVelocity: function(ax, ay, az, vx, vy, vz, dataFrequency) {
 
         velocity_x = vx + ax*dataFrequency;
@@ -31,46 +32,46 @@ module.exports = {
     estimateAttitude: function(){
         let Math = require('mathjs')
 
-        phi = Math.pi/3
-        theta = Math.pi/3
-        psi = Math.pi/4
+        phi = pi/3
+        theta = pi/3
+        psi = pi/4
         g = 9.81
         
 
-        C = [-Math.sin(theta), Math.sin(phi) * Math.cos(theta), Math.cos(phi)*Math.cos(theta)] // Cn_b * e_3
+        C = [-sin(theta), sin(phi) * cos(theta), cos(phi)*cos(theta)] // Cn_b * e_3
 
-        a_b = Math.multiply(-g, C)
+        a_b = multiply(-g, C)
         a_x = a_b[0]
         a_y = a_b[1]
         a_z = a_b[2]
 
         console.log(a_x)
 
-        phi_hat = Math.asin(a_x/g)
-        theta_hat = Math.atan(a_y/a_z)
+        phi_hat = asin(a_x/g)
+        theta_hat = atan(a_y/a_z)
 
-        D = (2 + 31/60 + 48/3600 )* (Math.pi/180)
-        I = (59 + 33/60 + 23/3600)* (Math.pi/180)
-        m = Math.sqrt((23660*Math.pow(10,-9))^2+(821*Math.pow(10,-9))^2+(40209*Math.pow(10,-9))^2)
+        D = (2 + 31/60 + 48/3600 )* (pi/180)
+        I = (59 + 33/60 + 23/3600)* (pi/180)
+        m = sqrt((23660*pow(10,-9))^2+(821*pow(10,-9))^2+(40209*pow(10,-9))^2)
 
-        m_n = Math.multiply([Math.cos(-I)*Math.cos(D), Math.cos(-I)*Math.sin(D),Math.sin(Math.abs(I))], m)
+        m_n = multiply([cos(-I)*cos(D), cos(-I)*sin(D),sin(abs(I))], m)
 
 
-        Cn_b = Math.matrix([[Math.cos(theta)*Math.cos(psi),Math.cos(theta)*Math.sin(psi),-Math.sin(theta)],
-            [Math.sin(phi)*Math.sin(theta)*Math.cos(psi)-Math.sin(psi)*Math.cos(phi), Math.sin(phi)*Math.sin(theta)*Math.sin(psi)+Math.cos(phi)*Math.cos(psi),  Math.sin(phi)*Math.cos(theta)],
-            [Math.cos(phi)*Math.sin(theta)*Math.cos(psi)+Math.sin(phi)*Math.sin(psi), Math.cos(phi)*Math.sin(theta)*Math.sin(psi)-Math.sin(phi)*Math.cos(psi), Math.cos(phi)*Math.cos(theta)]])
+        Cn_b = matrix([[cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta)],
+            [sin(phi)*sin(theta)*cos(psi)-sin(psi)*cos(phi), sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi),  sin(phi)*cos(theta)],
+            [cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi), cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi), cos(phi)*cos(theta)]])
         
-        m_b = Math.multiply(Cn_b, m_n)
+        m_b = multiply(Cn_b, m_n)
 
         m_x = m_b._data[0]
         m_y = m_b._data[1]
         m_z = m_b._data[2]
 
-        nom = Math.cos(phi_hat)*m_y-Math.sin(phi_hat)*m_z
-        denom = Math.cos(theta_hat)*m_x+Math.sin(phi_hat)*Math.sin(theta_hat)*m_y+Math.cos(phi_hat)*Math.sin(theta_hat)*m_z
+        nom = cos(phi_hat)*m_y-sin(phi_hat)*m_z
+        denom = cos(theta_hat)*m_x+sin(phi_hat)*sin(theta_hat)*m_y+cos(phi_hat)*sin(theta_hat)*m_z
 
 
-        psi_hat = D-Math.atan(Math.divide(nom,denom))
+        psi_hat = D-atan(divide(nom,denom))
         console.log(theta_hat)
         console.log(phi_hat)
         console.log(psi_hat)
