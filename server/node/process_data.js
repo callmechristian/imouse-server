e5f25754325ce032b85c401f074708aaacb5dd78
Christian Simăn-Cherecheș <49637374+callmechristian@users.noreply.github.com>
2022-09-27 10:30:48 +0200

    oh no

--- server/node/process_data.js
+++ server/node/process_data.js
@@ -2,6 +2,18 @@ const {pi, matrix, abs, sin, cos, sqrt, pow, atan, asin, multiply, divide} = req
 var log = require('./logging');
 const fs = require("fs");
 
+function mapYaw(psi_hat) {
+    
+
+    psi_hat += 1600
+
+    psi_hat *= 360/3200
+    
+    yaw = psi_hat-180
+
+    return yaw
+}
+
 module.exports = {   
     processAccellerationToVelocity: function(ax, ay, az, vx, vy, vz, dataFrequency) {
 
@@ -32,72 +44,178 @@ module.exports = {
         return _ret;
     },
 
-    estimateAttitude: function(a_y, a_x, a_z, m_y, m_x, m_z, g_x, g_y, g_z, dataFrequency, psi_hat){
+    // estimateAttitude: function(a_y, a_x, a_z, m_y, m_x, m_z, g_x, g_y, g_z, dataFrequency, psi_hat){
+    //     let Math = require('mathjs')
+
+    //     //normalize accelleration
+    //     g = 9.81;
+    //     a_x = a_x*g;
+    //     a_y = a_y*g;
+    //     a_z = a_z*g;
+
+    //     theta_hat = asin(a_x/g);
+    //     phi_hat = atan(a_y/a_z);
+
+    //     D = (2 + 31/60 + 48/3600 ) * (pi/180);
+
+    //     TtonT = pow(10,-9); //tesla to nanotesla
+    //     m_x = m_x*TtonT;
+    //     m_y = m_y*TtonT;
+    //     m_z = m_z*TtonT;
+
+    //     // nom = (cos(phi_hat)*m_y)-(sin(phi_hat)*m_z);
+    //     // denom = (cos(theta_hat)*m_x)+(sin(phi_hat)*sin(theta_hat)*m_y)+(cos(phi_hat)*sin(theta_hat)*m_z);
+
+
+    //     // psi_hat = D-atan(nom/denom)
+    //     // console.log(theta_hat)
+    //     // console.log(phi_hat)
+    //     // console.log(psi_hat)
+    //     // console.log("ax:" + a_x + "\n")
+    //     // console.log("m_x:" + m_x + ", m_y:" + m_y + ", m_z:" + m_z + "\n");
+
+    //     psi_hat = psi_hat + g_z*dataFrequency;
+    //     // console.log(g_z);
+
+    //     roll = phi_hat*180/pi;
+    //     pitch = theta_hat*180/pi;
+    //     yaw = psi_hat;
+
+    //     // var _raw = {
+    //     //     a_x: a_x,
+    //     //     a_y: a_y,
+    //     //     a_z: a_z,
+    //     //     m_x: m_x,
+    //     //     m_y: m_y,
+    //     //     m_z: m_z
+    //     // }
+    //     // fs.appendFile("../../logs/log_accel_magn_raw_1.json", JSON.stringify(_raw) + ",\n", (err) => {
+    //     //     if (err) {
+    //     //       console.log(err);
+    //     //     }
+    //     //     else {
+    //     //       // Get the file contents after the append operation
+    //     //       console.log("\nLogged:" + JSON.stringify(_raw));
+    //     //    }
+    //     // });
+
+    //     var _ret = {
+    //         roll: roll,
+    //         pitch: pitch,
+    //         yaw: yaw,
+    //         psi_hat: psi_hat
+    //     }
+        
+    //     return _ret
+    // },
+    estimateAttitude: function(a_x, a_y, a_z, m_x, m_y, m_z, g_x, g_y, g_z, psi_old, dt){
         let Math = require('mathjs')
 
-        //normalize accelleration
-        g = 9.81;
-        a_x = a_x*g;
-        a_y = a_y*g;
-        a_z = a_z*g;
+        phi = Math.pi/3
+        theta = Math.pi/3
+        psi = Math.pi/4
+        g = 9.81
+        
 
-        theta_hat = asin(a_x/g);
-        phi_hat = atan(a_y/a_z);
+        C = [-Math.sin(theta), Math.sin(phi) * Math.cos(theta), Math.cos(phi)*Math.cos(theta)] // Cn_b * e_3
 
-        D = (2 + 31/60 + 48/3600 ) * (pi/180);
-
-        TtonT = pow(10,-9); //tesla to nanotesla
-        m_x = m_x*TtonT;
-        m_y = m_y*TtonT;
-        m_z = m_z*TtonT;
-
-        // nom = (cos(phi_hat)*m_y)-(sin(phi_hat)*m_z);
-        // denom = (cos(theta_hat)*m_x)+(sin(phi_hat)*sin(theta_hat)*m_y)+(cos(phi_hat)*sin(theta_hat)*m_z);
+        // a_b = multiply(-g, C)
+        // a_x = a_b[0]
+        // a_y = a_b[1]
+        // a_z = a_b[2]
 
 
-        // psi_hat = D-atan(nom/denom)
-        // console.log(theta_hat)
-        // console.log(phi_hat)
-        // console.log(psi_hat)
-        // console.log("ax:" + a_x + "\n")
-        // console.log("m_x:" + m_x + ", m_y:" + m_y + ", m_z:" + m_z + "\n");
+        theta_hat = asin(-a_y*g/g)
 
-        psi_hat = psi_hat + g_z*dataFrequency;
-        console.log(g_z);
+        phi_hat = atan(-a_x/a_z)
 
-        roll = phi_hat*180/pi;
-        pitch = theta_hat*180/pi;
-        yaw = psi_hat;
+        D = (2 + 31/60 + 48/3600 )* (pi/180)
 
-        // var _raw = {
-        //     a_x: a_x,
-        //     a_y: a_y,
-        //     a_z: a_z,
-        //     m_x: m_x,
-        //     m_y: m_y,
-        //     m_z: m_z
+        m_y = -m_y
+        m_z = -m_z
+        
+
+        nom = cos(phi_hat)*m_y-sin(phi_hat)*m_z
+        denom = cos(theta_hat)*m_x+sin(phi_hat)*sin(theta_hat)*m_y+cos(phi_hat)*sin(theta_hat)*m_z
+
+        
+
+        // psi_hat = D-atan(divide(nom,denom))
+
+        psi_hat = psi_old + g_z * dt;
+        // console.log(psi_hat);
+        // console.log(g_z)
+        if (psi_hat < -1599)
+        {
+            psi_hat = -1599
+        }
+        if (psi_hat > 1600)
+        {
+            psi_hat = 1600
+        }
+        
+
+        roll = phi_hat*180/pi
+        pitch = theta_hat*180/pi
+        // yaw = psi_hat*180/pi
+        yaw = mapYaw(psi_hat)
+
+        // if (abs(yaw) > 180){
+        //     psi_hat = 0
+        //     yaw = 0
         // }
-        // fs.appendFile("../../logs/log_accel_magn_raw_1.json", JSON.stringify(_raw) + ",\n", (err) => {
-        //     if (err) {
-        //       console.log(err);
-        //     }
-        //     else {
-        //       // Get the file contents after the append operation
-        //       console.log("\nLogged:" + JSON.stringify(_raw));
-        //    }
-        // });
+
+        // roll = Math.round(roll * 100) / 100
+        // pitch = Math.round(pitch * 100) / 100
+        // yaw =Math.round(yaw * 100) / 100
+
+        roll = Math.round(roll)
+        pitch = Math.round(pitch)
+        yaw = Math.round(yaw)
+        yaw = psi_hat + 1600
+        yaw *= 800/3200
+        yaw = Math.floor(yaw)
+
+        // console.log(yaw);
 
         var _ret = {
             roll: roll,
             pitch: pitch,
             yaw: yaw,
-            psi_hat: psi_hat
+            psi: psi_hat
         }
         
         return _ret
     },
 
     calculateDisplacement: function(roll, pitch, yaw){
+        var displacement_x = pitch;
+        var displacement_y = yaw;
+        var displacement_z = roll; //not necessary
 
+        // filter parameters
+        var threshold_x = 10;
+        var threshold_y = 10;
+        var linear_slope_x = 1/2;
+        var linear_slope_y = 1/2;
+        
+        if(abs(pitch) <= threshold_y) {
+            displacement_y = 0;
+        }
+
+        if(abs(yaw) <= threshold_x) {
+            displacement_x = 0;
+        }
+
+        displacement_x = linear_slope_x*displacement_x;
+        displacement_y = linear_slope_y*displacement_y;
+
+
+        _ret = {
+            d_x: displacement_x,
+            d_y: displacement_y
+        }
+
+        return _ret;
     }
 };
--- server/node/server.js
+++ server/node/server.js
@@ -1,29 +1,31 @@
 var data = require('./process_data');
-var log1 = require('../../logs/usagelog.json');
-var meanOffset = require('./determine_offset');
-var log = require('./logging');
+const {getCursorPosition, setCursorPosition, sendCursorEvent, cursorEvents} = require("node-cursor");
+
+// var log1 = require('../../logs/usagelog.json');
+// var meanOffset = require('./determine_offset');
+// var log = require('./logging');
 
 //instantiate python shell with stdin to run in parallel
-let {PythonShell} = require('python-shell')
-let pyshell = new PythonShell('../python/movemouse.py');
+// let {PythonShell} = require('python-shell')
+// let pyshell = new PythonShell('../python/movemouse.py');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-const {exit} = require('process');
+// const {exit} = require('process');
 
 data.estimateAttitude(0,1,-1,20*Math.pow(10,-9),20*Math.pow(10,-9),20*Math.pow(10,-9))
 // exit()
 
 // sends a message to the Python script via stdin
-for (const a in log1){
-  var v = data.processAccellerationToVelocity(log1[a].x, log1[a].y, log1[a].z, 0, 0, 0, 60);
-  var d = data.estimateNewMouseDisplacement(0, 0, 0, v.vx, v.vy, v.vz, 1/60);
+// for (const a in log1){
+//   var v = data.processAccellerationToVelocity(log1[a].x, log1[a].y, log1[a].z, 0, 0, 0, 60);
+//   var d = data.estimateNewMouseDisplacement(0, 0, 0, v.vx, v.vy, v.vz, 1/60);
 
-  // console.log(d);
-  var str = Math.floor(d.x) + ' ' + Math.floor(d.y);
+//   // console.log(d);
+//   var str = Math.floor(d.x) + ' ' + Math.floor(d.y);
   
-  // pyshell.send(str);
-}
+//   // pyshell.send(str);
+// }
 
 
 //for debug purposes
@@ -34,41 +36,52 @@ pyshell.on('message', function (message) {
 });
 */
 
-var options = {
-  scriptPath: '../python/'
-};
+// var options = {
+//   scriptPath: '../python/'
+// };
 
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 7071 });
-const clients = new Map();
+// const clients = new Map();
 
 //timestamps
-var prevTime = Date.now();
+// var prevTime = Date.now();
 var dist_x = 0;
 var dist_y = 0;
 var psi_hat = 0;
 
+console.log("test");
+setCursorPosition({
+  x: 100,
+  y: 100
+});
+console.log(getCursorPosition());
+
 //called every 50ms
-function moveTheMouse() {
-  var str = Math.floor(dist_x) + ' ' + Math.floor(dist_y);
-  pyshell.send(str);
-  dist_x = 0;
-  dist_y = 0;
-}
+// function moveTheMouse() {
+//   // var str = Math.floor(dist_x) + ' ' + Math.floor(dist_y);
+//   // pyshell.send(str);
+//   setCursorPosition({
+//     x: dist_x,
+//     y: dist_y
+//   })
+//   dist_x = 0;
+//   dist_y = 0;
+// }
 
 // var offsets = meanOffset.getMeanOffsets();
 
 wss.on('connection', (ws) => {
-    const id = uuidv4();
-    const color = Math.floor(Math.random() * 360);
-    const metadata = { id, color };
+    // const id = uuidv4();
+    // const color = Math.floor(Math.random() * 360);
+    // const metadata = { id, color };
 
-    clients.set(ws, metadata);
+    // clients.set(ws, metadata);
 
     //only displace mouse every 50ms
-    setInterval(moveTheMouse, 50);
-    console.log('connected')
+    // setInterval(moveTheMouse, 50);
+    console.log('connected');
 
     ws.on('message', (messageAsString) => {
       time = Date.now();
@@ -78,7 +91,7 @@ wss.on('connection', (ws) => {
       const message = JSON.parse(messageAsString);
       // console.log(message)
       if(message != undefined) {
-        var att = data.estimateAttitude(message.x,message.y,message.z, message.m_x,message.m_y,message.m_z, message.g_x, message.g_y, message.g_z, dt/60, psi_hat)
+        var att = data.estimateAttitude(message.x,message.y,message.z, message.m_x,message.m_y,message.m_z, message.g_x, message.g_y, message.g_z, psi_hat, dt/60);
         // if (v.psi > 0.3){
           // console.log('roll: ', att.roll, 'pitch: ', att.pitch, 'yaw: ', att.yaw)
         psi_hat = att.psi_hat;
@@ -91,19 +104,24 @@ wss.on('connection', (ws) => {
         // //amplify displacement
         // dist_x = d.x*10000;
         // dist_y = d.y*10000;
-      }      
+      }     
+      
+      setCursorPosition(getCursorPosition() + {
+        x: dist_x,
+        y: dist_y
+      });
     });  
 });
 
 wss.on("close", () => {
-  clients.delete(ws);
+  // clients.delete(ws);
 });
 
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
+// function uuidv4() {
+//   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+//     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+//     return v.toString(16);
+//   });
+// }
 
 console.log("wss up");
